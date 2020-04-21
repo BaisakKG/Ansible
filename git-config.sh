c574c8982523f9447b1f38394eaf41337a0ea214
Baisak <baisak40@gmail.com>
2020-04-22 04:12:31 +0600

    add script

---
+++ git-config.sh
@@ -0,0 +1,3 @@
+git config --global --add user.email "baisak40@gmail.com"
+git config --global --add user.name "Baisak"
+
---
+++ install-bat-debian.sh
@@ -0,0 +1,4 @@
+wget https://github.com/sharkdp/bat/releases/download/v0.13.0/bat-musl_0.13.0_amd64.deb
+dpkg -i bat-musl_0.13.0_amd64.deb
+bat -h
+
