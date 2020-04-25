46a460408481d60a79d060aa7a05c8067e66553b
Baisak <baisak40@gmail.com>
2020-04-25 06:44:09 +0600

    Final course, create LXC/Docker

--- install-utils-debian.sh
+++ install-utils-debian.sh
@@ -1,4 +1,6 @@
 wget https://github.com/sharkdp/bat/releases/download/v0.13.0/bat-musl_0.13.0_amd64.deb
 dpkg -i bat-musl_0.13.0_amd64.deb
-bat -h
 
+apt install tree vagrant
+
+pip3 install argcomplete
